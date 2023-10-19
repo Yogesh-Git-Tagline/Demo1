3baae4fc68c29d701619d53b20a6ab0399532a53
YogeshC <yogeshc.tagline.com>
2023-10-19 17:33:10 +0530

    worked on 4th point

--- main.js
+++ main.js
@@ -1,50 +1,9 @@
 //js
 
 let data = [
-  {
-    name: 'ram',
-    email: 'ram@gmail.com',
-    gender: 'male',
-    hobbies: 'dancing',
-    age: 30,
-    dob: '15/03/2020',
-    country: 'india',
-    state: 'maharastra',
-    city: 'mumbai'
-  },
-  {
-    name: 'sham',
-    email: 'sham@gmail.com',
-    gender: 'male',
-    hobbies: 'singing',
-    age: 25,
-    dob: '25/05/2013',
-    country: 'india',
-    state: 'gujrat',
-    city: 'surat'
-  },
+
 
 ]
-
-
-function showdata() {
-  let i, ele;
-  let tbody = document.getElementById('tbl')
-
-  for (i = 0; i < data.length; i++) {
-    let trData = document.createElement('tr')
-
-    for (ele in data[i]) {
-      let tdData = document.createElement('td')
-      tdData.innerHTML = data[i][ele]
-      trData.appendChild(tdData)
-    }
-    tbody.appendChild(trData)
-  }
-
-}
-
-showdata()
 
 const CountryCityState = {
   "India": {
@@ -58,34 +17,101 @@ const CountryCityState = {
 }
 
 
-window.onload = function () {
-  let cont = document.getElementById("countries");
-  let state = document.getElementById("states");
-  let city = document.getElementById("cities");
-  let setCont, setState, setCity, i;
+let cont = document.getElementById("countries");
+let state = document.getElementById("states");
+let city = document.getElementById("cities");
+let setCont, setState, setCity;
 
-  for (setCont in CountryCityState) {
-    cont.options[cont.options.length] = new Option(setCont, setCont);
+for (setCont in CountryCityState) {
+  cont.options[cont.options.length] = new Option(setCont, setCont);
+}
+
+cont.onchange = function () {
+  state.length = 1;
+  city.length = 1;
+
+  for (setState in CountryCityState[this.value]) {
+    state.options[state.options.length] = new Option(setState, setState);
   }
+}
 
-  cont.onchange = function () {
-    state.length = 1;
-    city.length = 1;
+state.onchange = function () {
+  city.length = 1;
+  setCity = CountryCityState[cont.value][this.value];
 
-    for (setState in CountryCityState[this.value]) {
-      state.options[state.options.length] = new Option(setState, setState);
-    }
+  for (i = 0; i < setCity.length; i++) {
+    city.options[city.options.length] = new Option(setCity[i], setCity[i]);
   }
-
-  state.onchange = function () {
-    city.length = 1;
-    setCity = CountryCityState[cont.value][this.value];
-
-    for (i = 0; i < setCity.length; i++) {
-      city.options[city.options.length] = new Option(setCity[i], setCity[i]);
-    }
-  }
-
 }
 
 
+const form = document.querySelector('#myform')
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  let name = document.getElementById('name').value
+  let email = document.getElementById('email').value
+
+  let ch1 = document.getElementById('ch1')
+  let ch2 = document.getElementById('ch2')
+  let ch3 = document.getElementById('ch3')
+  let age = document.getElementById('age').value
+  let dob = document.getElementById('dob').value
+  let con = document.getElementById('countries').value
+  let state = document.getElementById('states').value
+  let city = document.getElementById('cities').value
+  let time=new Date().toLocaleTimeString()
+  let sethobbies = '', newGen, i;
+
+  let getGen = document.getElementsByName('gen')
+  for (i = 0; i < getGen.length; i++) {
+    if (getGen[i].checked) {
+      newGen = getGen[i].value
+    }
+  }
+
+  if (ch1.checked == true) {
+    sethobbies += ch1.value + ','
+  }
+  if (ch2.checked == true) {
+    sethobbies += ch2.value + ','
+  }
+  if (ch3.checked == true) {
+    sethobbies += ch3.value
+  }
+
+  data.push({
+    name: name,
+    email: email,
+    gender: newGen,
+    hobbies: sethobbies,
+    age: age,
+    dob: dob,
+    country: con,
+    state: state,
+    city: city,
+    time:time
+  })
+
+  showdata()
+  console.log(data)
+})
+
+function showdata() {
+  let ele;
+  let tbody = document.getElementById('tbl')
+
+  for (i = 0; i < data.length; i++) {
+    let trData = document.createElement('tr')
+
+    if (i === data.length - 1) {
+      for (ele in data[i]) {
+        let tdData = document.createElement('td')
+        tdData.innerHTML = data[i][ele]
+        trData.appendChild(tdData)
+      }
+      tbody.appendChild(trData)
+    }
+  }
+}
